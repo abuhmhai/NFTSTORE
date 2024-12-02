65df5387fe19ae1d30396aaaaaf3c7167b2a3074
Minh Hải <117704352+abuhmhai@users.noreply.github.com>
2024-12-02 15:03:32 +0700

    update all

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,4 +1,4 @@
-import Image from "next/image";
+ import Image from "next/image";
 import Header from "./components/header/Header";
 import styles from "./page.module.css";
 import Footer from "./components/footer/Footer";
--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.20",
   networks: {
     sepolia: {
       url: process.env.API_URL,
