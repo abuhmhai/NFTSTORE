5d42c0f9690745e029b15dbcf5038c65dbd91b40
Minh Hải <117704352+abuhmhai@users.noreply.github.com>
2024-12-03 16:21:13 +0700

    update footer flexible

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,7 +1,29 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ["ipfs.io", "gateway.pinata.cloud"],
+//   },
+// };
+//
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["ipfs.io", "gateway.pinata.cloud"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ipfs.io',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'gateway.pinata.cloud',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
